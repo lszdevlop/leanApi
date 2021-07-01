ce76e29ed3fe2ff3d838315022c8717b1aad2cb1
Lsz <lsztony000@gmail.com>
2021-07-01 22:56:42 +0800

    计算器页面

--- sample/views.py
+++ sample/views.py
@@ -8,8 +8,24 @@ def hello(request):
     name = request.GET.get("name")
     return HttpResponse("hello " + name)
 
+
 def calculator(request):
     """
     计算器视图
     """
-    return(render(request, "calculator.html"))
+    return (render(request, "calculator.html"))
+
+
+def calculate(request):
+    """
+    计算
+    """
+    num1 = request.GET.get("number1")
+    num2 = request.GET.get("number2")
+    if num1 == "" or num2 == "":
+        return HttpResponse("不能为空")
+    try:
+        result = int(num1) + int(num2)
+    except ValueError:
+        return HttpResponse("请输入数字")
+    return HttpResponse(result)
