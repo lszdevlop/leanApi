166661f9bc53703a491c69ea0bd6a0b4bf7efd8a
Lsz <lsztony000@gmail.com>
2021-07-10 23:35:55 +0800

    v1 api test

--- api/views.py
+++ api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.utils import timezone
 # Create your views here.
-def hello(request):
-    return JsonResponse({"sucess":"ok"})
+
+
+
+def users(request):
+    id = request.GET.get("id")
+    name = request.GET.get("name")
+    if id is None:
+        return JsonResponse({"sucess":"False", "message":"id is null"})
+    if name is None:
+        return JsonResponse({"sucess":"False", "message":"name is null"})
+    user = {"id":id,"name":name}
+    return JsonResponse({"sucess":"True", "message":"user info","data":user})
