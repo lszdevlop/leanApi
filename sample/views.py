9d9182bd78029313bb52c816b18e17b26f807241
Lsz <lsztony000@gmail.com>
2021-07-10 16:35:37 +0800

    修改post

--- sample/views.py
+++ sample/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 
 def hello(request):
@@ -28,14 +28,14 @@ def calculator(request):
 
 def calculate(request):
     """
-    计算
+    接口：计算
     """
-    # num1 = request.GET.get("number1")
-    # num2 = request.GET.get("number2")
-    # if num1 == "" or num2 == "":
-    #     return HttpResponse("不能为空")
-    # try:
-    #     result = int(num1) + int(num2)
-    # except ValueError:
-    #     return HttpResponse("请输入数字")
-    # return HttpResponse(result)
+    num1 = request.GET.get("number1","")
+    num2 = request.GET.get("number2","")
+    if num1 == "" or num2 == "":
+        return JsonResponse({"code":"10101","message":"参数不能为空", "data":[]})
+    try:
+        result = int(num1) + int(num2)
+    except ValueError:
+        return JsonResponse({"code":"10102","message":"请输入数字", "data":[]})
+    return JsonResponse({"code":"10200","message":"成功", "data":ret})
